157102716c48c8eec263914daf305b175c94eeb4
bczrsk <105373534+bczrsk@users.noreply.github.com>
2025-04-08 22:58:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/pogoda/service-worker.js')
+  }
+
+const apiKey = "7de9d5d5ddfda64e40d68815c9abfa80"; // Twój klucz API
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+// Funkcja do sprawdzania pogody na podstawie miasta
+async function CheckWeather(miasto) {
+    // Tworzymy pełny URL zapytania
+    const response = await fetch(apiUrl + miasto + `&appid=${apiKey}`);
+    const data = await response.json(); // Pobieramy dane z API
+
+    // Pobieranie danych z odpowiedzi API
+    const city = data.name;
+    const temperature = data.main.temp;
+    const wind = data.wind.speed;
+    const humidity = data.main.humidity;
+
+    // Wyświetlanie wyników na stronie
+    document.getElementById("nazwa-miasta").textContent = `Miasto: ${city}`;
+    document.getElementById("temperatura").textContent = `Temperatura: ${temperature}°C`;
+    document.getElementById("wiatr").textContent = `Wiatr: ${wind} km/h`;
+    document.getElementById("wilgotnosc").textContent = `Wilgotność: ${humidity}%`;
+
+    // Pokazanie sekcji z wynikami pogody
+    document.getElementById("pogoda").style.display = "block";
+}
+
+// Funkcja uruchamiająca po kliknięciu przycisku
+function searchWeather() {
+    const miasto = document.getElementById("miasto").value; // Pobranie miasta z inputu
+    CheckWeather(miasto); // Wywołanie funkcji sprawdzającej pogodę
+
+}
